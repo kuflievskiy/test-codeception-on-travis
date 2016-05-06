6ace5d93d85fcd828956a0dd3e92262ffa5d24c1
Aleksey Kuflievskiy <kuflievskiy@gmail.com>
2016-05-06 19:03:34 +0300

    new files

--- tests/acceptance/IndexCept.php
+++ tests/acceptance/IndexCept.php
@@ -2,5 +2,5 @@
 $I = new AcceptanceTester( $scenario );
 $I->am( 'A standard user' );
 $I->wantTo( 'ensure that I can see the Index page' );
-$I->amOnPage( '/index.html' );
+$I->amOnPage( '/' );
 $I->see( 'Lorem Ipsum' );
