50c20a4ac085dbdd37e04e1154baf9f3ba968117
kuflievskiy <kuflievskiy@gmail.com>
2016-05-07 00:42:05 +0300

    added travis-ci-apache

--- tests/acceptance/IndexCept.php
+++ tests/acceptance/IndexCept.php
@@ -1,5 +1,5 @@
 <?php
 $I = new AcceptanceTester( $scenario );
 $I->wantTo( 'ensure that I can see the Index page' );
-$I->amOnPage( '/index.html' );
+$I->amOnPage( '/' );
 $I->see( 'Lorem Ipsum' );
